c86e0cd16325065d3f2ed301b5b91f4c1d8e3143
Gabriele Bovolenta <101414963+gabriele-bovolenta@users.noreply.github.com>
2022-11-29 22:14:36 +0100

    add login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { Injectable, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.components';
 import { DetailsComponent } from './components/details/details.component';
 import { HomeComponent } from './components/home/home.component';
 import { LookUpDrinkByIdResolver } from './resolver/lookup-drink';
 
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
   { path: "home", component: HomeComponent},
   { path: "home/:search", component: HomeComponent},
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { LoginComponent } from './components/login/login.components';
+import { HomeComponent } from './components/home/home.component';
 import { DrinkLiComponent } from './components/shared/drink-li/drink-li.component';
 import { DetailsComponent } from './components/details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
     HomeComponent,
     DrinkLiComponent,
     DetailsComponent
